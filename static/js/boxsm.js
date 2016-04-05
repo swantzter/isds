908b4c5c72719aab1b41224a8d26b0556912b360
Svante Bengtson <svante.b37@gmail.com>
2016-04-05 23:13:10 +0200

    Move boxsm to js

---
+++ static/js/boxsm.js
@@ -0,0 +1,41 @@
+var content = `
+This specification is aimed to give Ops a better overlook of how their field agents have deployed the involved portals, or to be able to give simple instructions to agents on field how to deploy the resonators and mods.
+            <table>
+                <tr>
+                    <td>Currently displaying:</td>
+                    <td>v2.2</td>
+                </tr>
+                <tr>
+                    <td>Latest Version:</td>
+                    <td id="latest">v2.2</td>
+            </table>
+            <h2>Links</h2>
+            <ul class="ulno">
+              <li>
+                  Other Versions
+                  <ul class="ulno" id="links">
+                      <li><a href="#">Version 2.2</a> (please enable Javascript to see full list)</li>
+                  </ul>
+              </li>
+              <li>
+                  ISDS generator - TBD
+              </li>
+              <li>
+                  <a href="https://github.com/svbeon/isds">GitHub</a>
+              </li>
+              <li>
+                  <a href="ircs://chat.freenode.net:6697/isds">#isds on freenode</a>
+              </li>
+              <li>
+                  <a href="https://www.dropbox.com/request/QvA5QHN7RbM7RbPfAzrr" target="_blank">Contribute with Examples</a>
+                  <ul class="ulno">
+                      <li>Please upload only Screenshots of a portal &#8209;&gt; Deploy status, take the screenshot just before the agent names appear. Or screenshots of a portal &#8209;&gt; Mods. All personal identifyable information will be removed before publishing and by uploading an image you agree that it'll be licensed under CC-0 1.0 International.</li>
+                  </ul>
+              </li>  
+         </ul>
+        <span class="spec noborder">Not officially affiliated with Ingress or Niantic Labs.</span>
+       `;
+
+(function() {
+  document.getElementsByClassName("box sm")[0].innerHTML = content;
+})();
